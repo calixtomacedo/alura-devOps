bb3e3a7019ed7997864744992e7aa24785177f77
Calixto Macedo <calixto.macedo@gmail.com>
2021-10-09 20:25:09 -0300

    Ajustes

--- curso-de-terraform/terraform/main.tf
+++ curso-de-terraform/terraform/main.tf
@@ -8,7 +8,9 @@ resource "aws_instance" "dev" {
     ami = "ami-09e67e426f25ce0d7"
     instance_type = "t2.micro"
     key_name = "terraform-aws"
+
     tags = {
        Name = "dev${count.index}" 
     }
+    
 }
