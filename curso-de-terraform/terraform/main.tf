5dbf5d14aa229c2dae692ddf5b422cce32cf8e55
Calixto Macedo <calixto.macedo@gmail.com>
2021-10-13 07:50:57 -0300

    02.Modificando a infraestrutura e adicionando novos recursos

--- curso-de-terraform/terraform/main.tf
+++ curso-de-terraform/terraform/main.tf
@@ -1,3 +1,4 @@
+/*
 provider "aws" {
     #version = "~> 3.27"
     region  = "us-east-1"
@@ -13,4 +14,56 @@ resource "aws_instance" "dev" {
        Name = "dev${count.index}" 
     }
     
+}
+
+
+
+resource "aws_security_group" "acesso-ssh" {
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+  
+  ingress =  {
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["191.183.201.237/22"]
+    }  
+
+  tags = {
+    Name = "ssh"
+  }
+}
+*/
+
+provider "aws" {
+  #version = "~> 2.0"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "dev" {
+  count = 3
+  ami = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+    Name = "dev${count.index}"
+  }
+  vpc_security_group_ids = ["sg-0494f947ad5a9858c"]
+}
+
+resource "aws_security_group" "acesso-ssh" {
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["191.183.201.237/32"]
+  }
+  tags = {
+    Name = "ssh"
+  }
 }
